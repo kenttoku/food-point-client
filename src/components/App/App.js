12f19efb3d24f4e4c2af348f758a76b91234511e
Kent Tokunaga <KentTokunaga@gmail.com>
2019-01-12 19:57:26 -0500

    get token on App component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,18 +1,29 @@
-import React from 'react';
+import React, { Component } from 'react';
 import Login from '../Login';
 import Signup from '../Signup';
 import Dashboard from '../Dashboard';
 
-const App = () => (
-  <>
-    <h1>Authentication Template</h1>
-    <h2>Signup</h2>
-    <Signup />
-    <h2>Login</h2>
-    <Login />
-    <h2>Content</h2>
-    <Dashboard />
-  </>
-);
+class App extends Component {
+  componentDidMount() {
+    const token = localStorage.getItem('authToken');
+    if (token) {
+      console.log(token);
+    }
+  }
+
+  render() {
+    return (
+      <>
+        <h1>Authentication Template</h1>
+        <h2>Signup</h2>
+        <Signup />
+        <h2>Login</h2>
+        <Login />
+        <h2>Content</h2>
+        <Dashboard />
+      </>
+    );
+  }
+}
 
 export default App;
