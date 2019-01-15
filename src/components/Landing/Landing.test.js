f7e92709fa729f14e84d2e5c668d28e48cd1cee6
Kent Tokunaga <KentTokunaga@gmail.com>
2019-01-15 18:32:47 -0500

    Create landing component

---
+++ src/components/Landing/Landing.test.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import Landing from './Landing';
+
+describe('<Landing />', () => {
+  it('Renders without crashing', () => {
+    shallow(<Landing />);
+  });
+});
